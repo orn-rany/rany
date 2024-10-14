51940ee5b2b8b365c54f0b32b9b1e7d786acacc1
orn-rany <ranyorn@gmail.com>
2024-10-14 08:01:06 -0700

    hello

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello, My name shy girl");
+        System.out.printf("Hello, My name shy girl");
     }
 }
